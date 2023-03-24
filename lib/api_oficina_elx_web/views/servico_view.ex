18e9ea46ca72e7f913335147b331f4602b868001
pedrohms <pedromartins12117@gmail.com>
2023-03-23 22:02:04 -0300

    donos and carros in json

--- lib/api_oficina_elx_web/views/servico_view.ex
+++ lib/api_oficina_elx_web/views/servico_view.ex
@@ -2,6 +2,10 @@ defmodule ApiOficinaElxWeb.ServicoView do
   use ApiOficinaElxWeb, :view
   alias ApiOficinaElxWeb.ServicoView
 
+  def render("indexdc.json", %{servicos: servicos}) do
+    %{data: render_many(servicos, ServicoView, "servicodc.json")}
+  end
+
   def render("index.json", %{servicos: servicos}) do
     %{data: render_many(servicos, ServicoView, "servico.json")}
   end
@@ -15,6 +19,25 @@ defmodule ApiOficinaElxWeb.ServicoView do
       id: servico.id,
       dono_id: servico.dono_id,
       carro_id: servico.carro_id,
+      data_entrega: servico.data_entrega,
+      preco: servico.preco,
+      observacao: servico.observacao
+    }
+  end
+
+  def render("servicodc.json", %{servico: servico}) do
+    %{
+      id: servico.id,
+      dono: %{
+        id: servico.dono.id,
+        nome: servico.dono.nome,
+        numero_celular: servico.dono.numero_celular
+      },
+      carro: %{
+        id: servico.carro.id,
+        nome: servico.carro.nome,
+        cor: servico.carro.cor
+      },
       data_entrega: servico.data_entrega,
       preco: servico.preco,
       observacao: servico.observacao
